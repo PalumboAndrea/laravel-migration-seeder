ef12524bc5e1a72ed77445db2d9b88a328332794
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-02-10 17:38:53 +0100

    add model, controller and db datas on page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <h1 class="m-4">
+        Treni in partenza:
+    </h1>
+    
+    <div class="container">
+        <div class="row">
+            @foreach ($trains as $train)
+            <div class="col-4">
+                <span>
+                    Partenza da:
+                    <h3 class="d-inline">
+                        {{ $train->stazione_di_partenza }}
+                    </h3>
+                </span>
+                <p>
+                    Azienda: {{ $train->azienda }}
+                </p>
+                <p>
+                    Orario di partenza: {{ $train->orario_di_partenza }}
+                </p>
+                <p>
+                    Orario di arrivo previsto: {{ $train->orario_di_arrivo }}
+                </p>
+                <p>
+                    In orario: @if($train->in_orario) si @else no @endif
+                </p>
+            </div>
+            @endforeach
+        </div>
+    </div>
+    
+
+</body>
+
+</html>
